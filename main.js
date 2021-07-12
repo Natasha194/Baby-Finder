271dac8129b76d5a74922ed1fdfa247bcc10ea5e
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-07-12 18:39:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 status = "";
 img = "";
 objects = [];
+song = "";
 
 function setup() {
     canvas = createCanvas(380 ,380);
@@ -12,32 +13,40 @@ function setup() {
     document.getElementById('status').innerHTML = "Status: Detecting Object";
 }
 
-function draw() {
+function preload() {
+    song = loadSound("Madilyn bailey - view.mp3");
+}
 
-    image(video, 0, 0, 380, 380);
-    objectDetector.detect(video, gotResult);
+function draw() { 
+    image(video, 0, 0, 380, 380); if(status != "") { 
+        r = random(255); 
+        g = random(255); 
+        b = random(255); 
+        objectDetector.detect(video, gotResult); 
+        for (i = 0; i < objects.length; i++) { 
+            document.getElementById("status").innerHTML = "Status : Object Detected"; 
+            fill(r,g,b); percent = floor(objects[i].confidence * 100); 
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15); 
+            noFill(); 
+            stroke(r,g,b); 
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+            if(objects[i].label == "person") { 
+                document.getElementById("number_of_objects").innerHTML = "Baby Found"; 
+                console.log("stop"); 
+                song.stop(); 
+            } 
+            else { 
+                document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+                console.log("play");
+            }
 
-    if (status != "") {
-        r = random(255);
-        g = random(255);
-        b = random(255);
-        for (i = 0; i < objects.length; i++) {
-            document.getElementById('status').innerHTML = 'Status: Object Detected';
-            document.getElementById('OI').innerHTML = "Number of Objects: " + objects.length;
 
-            fill(r, g, b);
-            percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + " " + percent +"%", objects[i].x + 15, objects[i].y + 15);
-            noFill();
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].width);
+            if(objects.length == 0) { 
+                document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+                console.log("play"); 
+                song.play(); 
+            }
         }
-    }
-
-    if(objects.label != 'person') {
-        document.getElementById('audio').play();        
-    }
-    else {
-        document.getElementById('audio').stop();
     }
 }
 
